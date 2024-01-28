42359ee4696cbc505fca2b4bd48555620cfc0745
OfficialCodes101 <custofeng159@gmail.com>
2024-01-28 21:36:27 +0100

    progress

---
+++ index.js
@@ -0,0 +1,61 @@
+const {
+  DisconnectReason,
+  useMultiFileAuthState,
+  makeInMemoryStore,
+  jidDecode
+} = require("@whiskeysockets/baileys");
+const makeWASocket = require("@whiskeysockets/baileys").default;
+const { processIncomingMessages } = require('./processMessages');
+
+
+async function connectionLogic() { 
+  const { state, saveCreds } = await useMultiFileAuthState("auth_info_baileys");
+  const store = makeInMemoryStore({});
+  // can be read from a file
+  store.readFromFile("./baileys_store.json");
+  // saves the state to a file every 10s
+  setInterval(() => {
+    store.writeToFile("./baileys_store.json");
+  }, 10_000);
+  const sock = makeWASocket({
+    // can provide additional config here
+    printQRInTerminal: true,
+    auth: state,
+  });
+
+  store.bind(sock.ev);
+
+  sock.ev.on("connection.update", async (update) => {
+    const { connection, lastDisconnect, qr } = update || {};
+
+    if (qr) {
+      console.log(qr);
+      // write custom logic over here
+    }
+
+    if (connection === "close") {
+      const shouldReconnect =
+        lastDisconnect?.error?.output?.statusCode !==
+        DisconnectReason.loggedOut;
+
+      if (shouldReconnect) {
+        connectionLogic();
+      }
+    }
+  });
+
+  sock.ev.on("messages.update", (messageInfo) => {
+    console.log(messageInfo);
+  });
+
+  
+
+  sock.ev.on("messages.upsert", async (messageInfoUpsert) => {
+    console.log(`${JSON.stringify(messageInfoUpsert.messages[0], null, 2)}`);
+    await processIncomingMessages(sock, messageInfoUpsert);
+  });
+  sock.ev.on("creds.update", saveCreds);
+}
+
+connectionLogic();
+
---
+++ processMessages.js
@@ -0,0 +1,102 @@
+const XRegExp = require('xregexp');
+const commands = require('./lib/plugins')
+
+
+module.exports.processIncomingMessages = async (sock, messageInfoUpsert) => {
+    if (messageInfoUpsert.hasOwnProperty('messages')) {
+        for (msg of messageInfoUpsert.messages) {
+            let message_array = commands.getTitle(msg);
+            if (!message_array) {
+                console.log('Not a text message');
+                continue;
+            }
+            message_array = message_array.trim().split(' ');
+            /*
+            try {
+                message_array = msg.message.extendedTextMessage.text.trim().split(' ');
+            } catch (error) {
+                message_array = msg.message.conversation.trim().split(' ');
+            }
+            */
+            let message = message_array.filter(element => element !== '').join(' ');
+            console.log(message);
+        
+            const prompt_pattern = /^\.(?:\s)?(\w+)(?: ?(.*))?/g;
+            const matches = XRegExp.exec(message, prompt_pattern);
+        
+            if (!matches) {
+                if (msg.key.fromMe) {
+                    console.log('From me');
+                    continue;
+                }
+                continue;
+            }
+            console.log(matches);
+        
+            let command = '';
+            try {
+                command = matches[1];
+            } catch (error) {
+                sock.sendMessage(msg.key.remoteJid, {
+                    text: 'Request Failed'
+                })
+            }
+
+            const data = {
+                text: message,
+                key: msg.key,
+                message: msg.message
+            }
+            
+            switch (command.toLowerCase()) {
+                case 'greet':
+                    await commands.greet(sock, data);
+                    continue;
+                // case 'spam':
+                //     const str = matches[2];
+                //     await commands.spam(sock, data, str);
+                //     continue;
+                case 'lyrics':
+                    await commands.lyrics(sock, data, matches[2].trim());
+                    continue;
+                case 'add':
+                    await commands.add(sock, data, matches[2].trim())
+                    continue;
+                case 'kick':
+                    await commands.kick(sock, data, matches[2]);
+                    continue;
+                case 'promote':
+                    await commands.promoteOrDemote(sock, data, matches[2], 'promote');
+                    continue;
+                case 'demote':
+                    await commands.promoteOrDemote(sock, data, matches[2], 'demote');
+                    continue;
+                case 'mute':
+                    await commands.muteUnmute(sock, data, 'announcement');
+                    continue;
+                case 'unmute':
+                    await commands.muteUnmute(sock, data, 'not_announcement');
+                    continue;
+                case 'tag':
+                    await commands.tag(sock, data, matches[2]);
+                    continue;
+                case 'tagall':
+                    await commands.tag(sock, data, 'all');
+                    continue;
+                default:
+                    await sock.sendMessage(data.key.remoteJid, {
+                        text: 'Not implemented yet, idiot',
+                        contextInfo: commands.contextParser(data)
+                    })
+            }
+            /*
+            if (matches) {
+                matches.forEach(elem => console.log(elem));
+            }
+            */
+        }
+    }
+
+}
+
+
